b7e4fccf19bafb8c708a489c8cda6360edf77681
Cassidy Johnson <cassidyrose56@gmail.com>
2022-10-24 09:23:15 -0500

    styling on upload.js

--- client/components/upload.js
+++ client/components/upload.js
@@ -1,4 +1,5 @@
 import React, {useState, useRef} from 'react';
+import '../styles/upload.scss'
 
 
 export default function Upload(props){
@@ -32,20 +33,20 @@ export default function Upload(props){
 // file popup window not showing when i click the button. Can someone else try it on their comp?  Thanks
 // also cant type in the input fields.  Gonna hit up help desk for that cuz it seems like everything is correct
 return(
-<div>
-    <h1>CREW IT UP</h1>
+<div id='upload-layout'>
+    <h1 id='upload-title'>Update your account</h1>
     <form id='user-setup' onSubmit={handleSubmit}>
-        <div id='name-bar'>Name:
+        <p id='name-bar' className='label-text'>Name<br/></p>
             <input type='text'
             name='name'
-            className='name-input'
+            className='input-style'
             value={name}
             onChange={(e) => setName(e.target.value)}
             /> 
-        </div>
-        <label id='city-dropdown'>
-            <span>City:</span>
-            <select id='select-city' value={city} onChange={(e) => setCity(e.target.value)}>
+        
+        {/* <label id='city-dropdown'> */}
+            <p className='label-text'>City<br/></p>
+            <select id='select-city' value={city} className='input-style' onChange={(e) => setCity(e.target.value)}>
                 <option value='new york'>New York</option>
                 <option value='london'>London</option>
                 <option value='los angeles'>Los Angeles</option>
@@ -54,33 +55,28 @@ return(
                 <option value='chicago'>Chicago</option>
                 <option value='new orleans'>New Orleans</option>
             </select>
-        </label> 
-        <div id='email-bar'>
-            <span>Email:</span>
+            <p className='label-text'>Email<br/></p>
             <input type='email'
             name="email"
-            className="email-input"
+            className='input-style'
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
             />
-        </div>
-        <div id='bio-text'>
-            <p>Bio</p>
+            <p className='label-text'>Bio</p>
             <input type='text'
             name='bio'
-            className='bio-input'
+            className='input-style'
             maxLength='800'
             value={bio}
             onChange={(e) => setBio(e.target.value)}
             />
-        </div>
-        <button  id='upload-button' onClick={handleClick}> Upload a file </button>  
+        <button id='upload-button' className='button-style' onClick={handleClick}>Upload a file</button>  
         <input type="file"
             ref={hiddenFileInput}
             onChange={handleChange}
             style={{display:'none'}} 
             />
-        <button type='submit' id='set-user-btn'>Submit</button>
+        <button type='submit' className='button-style' id='set-user-btn'>Submit</button>
     </form>
 </div>  
 )
