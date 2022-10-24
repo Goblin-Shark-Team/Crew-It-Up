3b2b355dc4e0feedad37696d2380a7fb0a1b5aa2
Katharine Angelopoulos <katharineangelopoulos@Katharines-Air.attlocal.net>
2022-10-24 15:20:41 -0500

    fixes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,7 @@
 import React, { Component, useState, useEffect  } from 'react';
-import Login from './Login'
-import Portfolio from './Portfolio';
 import '../styles/portfolio.scss';
-<<<<<<< HEAD
 import Gallery from './Gallery.jsx';
-
-function App() {
+export default function App() {
   const [user, setUser] = useState({});
   console.log(user);
   return (
@@ -13,27 +9,7 @@ function App() {
       <Gallery />
     </div>
   )
-};
-
-export default App;
-
-=======
-import Header from './Header';
-
-import Signup from './Signup'
-import Upload from './upload';
-import Navbar from './Navbar';
-
-
-function App() {
-  const [user, setUser] = useState({});
-  console.log(user);
-  return (
-    <div>
-      <Header setUser={setUser}/>
-    </div>
-  )
 
 }
->>>>>>> dev
+
 
--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+
 
 export default function Gallery () {
   
